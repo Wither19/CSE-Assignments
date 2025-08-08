5f0ea7d56ab5ec869c201cc03a8db54a278fb8c0
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-08-08 12:45:06 -0400

    Add files via upload

---
+++ CSA/YourAge/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("What's your name? ");
+    String name = s.nextLine();
+
+    System.out.println("Ok, " + name + ", How old are you? ");
+    int age = s.nextInt();
+
+    if (age < 16) {
+      System.out.println("You can't drive.");
+    }
+    if (age < 18) {
+      System.out.println("You can't vote.");
+    }
+    if (age < 25) {
+      System.out.println("You can't rent a car.");
+    }
+    if (age >= 25) {
+      System.out.println("You can do anything that's legal.");
+    }
+  }
+}
